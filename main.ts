667110722527e2a1bb9e62a82e181ed557791ccf
Luna Nyx <79994302+Cloise10@users.noreply.github.com>
2021-03-03 13:34:22 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+let strike = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(strike)
+    strike += 1
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+basic.forever(function () {
+	
+})
